58935dc554583ee5cc28fea5cb61bbc30811e896
yeong723 <135084831+yeong723@users.noreply.github.com>
2023-05-31 11:07:03 +0900

    Add files via upload

---
+++ 1522.py
@@ -0,0 +1,9 @@
+def s():
+    for i in range(5):
+        for j in range(5):
+            if j<i:
+                print(' ',end='')
+            else:
+                print('*',end='')
+        print()
+print(s())
